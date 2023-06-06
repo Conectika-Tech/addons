e6d139667c40459b322b4168ecf397d8efbd82dd
Jericho Ruz <potosho@gmail.com>
2023-06-05 19:32:24 -0600

    feat(cfdi downloader): new function in odoo

---
+++ conectika_tech/__manifest__.py
@@ -0,0 +1,32 @@
+###################################################################################
+#
+#    Conéctika.Tech
+#    Copyright (C) 2023-TODAY Conéctika.Tech (<http://conectika.tech>).
+#
+#    Author: Jericho Ruz
+#
+###################################################################################
+
+{
+    'name': 'Conectika Tech',
+    'summary': 'CFDI Downloader and PDF validations',
+    'version': '16.0.1',
+    'author': 'Conectika.tech',
+    'website': "https://conectika.tech",
+    'company': 'Conectika.tech',
+    'maintainer': 'Conectika.tech',
+    'live_test_url':
+        'https://conectika.tech',
+    "category": "Industries",
+    "depends" : ["l10n_mx_edi",],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/res_partner_data.xml',
+        'wizard/cfdi_downloader_view.xml',
+        'views/partner_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+}
---
+++ conectika_tech/wizard/__init__.py
@@ -0,0 +1,3 @@
+from . import cfdi_downloader
+
+
